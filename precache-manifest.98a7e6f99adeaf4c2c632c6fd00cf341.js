9caeff054d0f4acbef7f01cab4805774aa9ca64a
hafizalasad123 <hafizasad06@gmail.com>
2020-09-06 14:44:30 +0600

    Updates

---
+++ precache-manifest.98a7e6f99adeaf4c2c632c6fd00cf341.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35569de061a03c731035dc2dfb23e250",
+    "url": "/hafizalasad121/Rice-Disease-detector/index.html"
+  },
+  {
+    "revision": "3b4128662c2a4b8839f2",
+    "url": "/hafizalasad121/Rice-Disease-detector/static/css/2.ff8466aa.chunk.css"
+  },
+  {
+    "revision": "76c5e79d1904981fa222",
+    "url": "/hafizalasad121/Rice-Disease-detector/static/css/main.3e9f870c.chunk.css"
+  },
+  {
+    "revision": "3b4128662c2a4b8839f2",
+    "url": "/hafizalasad121/Rice-Disease-detector/static/js/2.4e8a2924.chunk.js"
+  },
+  {
+    "revision": "647c25ca9e3ea0b71bfc55fa250b5834",
+    "url": "/hafizalasad121/Rice-Disease-detector/static/js/2.4e8a2924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76c5e79d1904981fa222",
+    "url": "/hafizalasad121/Rice-Disease-detector/static/js/main.bb019eb3.chunk.js"
+  },
+  {
+    "revision": "13f32c3321923f6fb2a1",
+    "url": "/hafizalasad121/Rice-Disease-detector/static/js/runtime-main.0e93b6dd.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/hafizalasad121/Rice-Disease-detector/precache-manifest.98a7e6f99adeaf4c2c632c6fd00cf341.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hafizalasad121/Rice-Disease-detector/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
